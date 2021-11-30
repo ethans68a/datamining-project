c122b007c2914556a67cb7018d0de4d051d274f6
ethans68a <61880553+ethans68a@users.noreply.github.com>
2021-11-29 21:18:05 -0500

    finalize data preprocessing again

--- preprocessing.py
+++ preprocessing.py
@@ -24,24 +24,28 @@ def clean_genres(orig_val):
     if "punk" in orig_val or (orig_val == "dutch prog"):
         return "rock"
 
-    if orig_val == "motown" or orig_val == "adult standards":
+    if (orig_val == "motown") or (orig_val == "adult standards"):
         return "oldies"
 
     if "rap" in orig_val:
         return "hip hop"
 
-    return orig_val
+    return None
 
 
 spotify_data = pd.read_csv("csv/Spotify-2000.csv")
-# print(spotify_data["TopGenre"])
-print(len(pd.unique(spotify_data["TopGenre"])))
-pd.DataFrame(pd.unique(spotify_data["TopGenre"])).to_html(
-    "html/unique-genres.html", index=False)
 
+# apply function to each record, apply to TopGenre column
 spotify_data["TopGenre"] = spotify_data["TopGenre"].apply(clean_genres)
-print(spotify_data["TopGenre"])
-print(pd.unique(spotify_data["TopGenre"]))
+
+# remove songs that we were unable to manually narrow down
+spotify_data = spotify_data.dropna()
+spotify_data = spotify_data.drop(axis=1,columns=['Index'])
+
+# verify shape
+print(spotify_data.shape)
+
+# output to HTML files for presentation and to CSV
 
 spotify_data.head().to_html("html/head.html", index=False)
 spotify_data.to_csv("csv/spotify-2000-clean.csv", index=False)
