75315a1e522c3f9da66cda247963d3141f99d4c0
ethans68a <61880553+ethans68a@users.noreply.github.com>
2021-12-07 04:18:50 -0500

    add some R stuff for classification

---
+++ multiple-linear-regression.R
@@ -0,0 +1,40 @@
+library(leaps)
+
+setwd("~/Documents/Fall-2021/datamining/spotify-project")
+spotify_clean <- read.csv("csv/spotify-2000-clean.csv")
+spotify_df <- data.frame(spotify_clean)
+spotify_tr_df <- spotify_df[1:1200, ]
+spotify_test_df <- spotify_df[1201:nrow(spotify_df), ]
+
+# remove irrelevant attributes
+spotify_tr_df_valence <-
+  subset(spotify_tr_df,
+         select = -c(Title, Artist, Year, Popularity, TopGenre))
+
+# multiple linear regression, numeric only - predict valence
+# need to verify LINE assumptions and also do cross validation to confirm
+# also do variable selection
+
+m1 <- lm(Valence ~ ., data = spotify_tr_df_valence)
+print(summary(m1))
+plot(m1$res ~ m1$fitted)
+
+# plot residuals vs Xi1 ... Xik
+names_clean <- setdiff(names(spotify_tr_df_valence), c("Valence"))
+for (colname in names_clean) {
+  plot(
+    m1$res ~ spotify_tr_df_valence[, colname],
+    main = paste("Residuals vs", colname),
+    ylab = "Residuals",
+    xlab = colname
+  )
+}
+
+X <- spotify_tr_df_valence[, !names(spotify_tr_df_valence) %in% c("Valence")]
+y <- spotify_tr_df_valence[, "Valence"]
+out <-
+  summary(regsubsets(X, y, nbest = 2, nvmax = ncol(X))) # 'nbest' is the number of top models shown for subset size
+tab1 <-
+  cbind(out$which, out$rsq, out$adjr2, out$cp) # organize the results into a table
+print(tab1)
+step(m1)
--- preprocessing.py
+++ preprocessing.py
@@ -4,16 +4,16 @@ import csv
 import itertools
 from sklearn.model_selection import train_test_split
 
+
 def str_to_int_cleaner(orig_val):
     try:
         val_as_int = int(orig_val)
         return val_as_int
     except ValueError as e:
-        return orig_val 
+        return orig_val
 
 
-
-def del_unwanted_char(orig_val_param,what_char='"'):
+def del_unwanted_char(orig_val_param, what_char='"'):
     if isinstance(orig_val_param, str):
         # orig_val = orig_val_param.replace("'", "")
         orig_val = orig_val_param.replace(what_char, "")
@@ -58,7 +58,8 @@ spotify_data = pd.read_csv("csv/Spotify-2000.csv")
 
 # apply function to each record, apply to TopGenre column
 spotify_data["TopGenre"] = spotify_data["TopGenre"].apply(clean_genres)
-spotify_data["Length (Duration)"] = spotify_data["Length (Duration)"].apply(del_unwanted_char,args=(','))
+spotify_data["Length (Duration)"] = spotify_data["Length (Duration)"].apply(
+    del_unwanted_char, args=(','))
 
 # fix characters
 spotify_data = spotify_data.applymap(del_unwanted_char)
@@ -79,7 +80,7 @@ spotify_data.to_csv("csv/spotify-2000-clean.csv",
                     index=False, quoting=csv.QUOTE_NONNUMERIC)
 spotify_data.to_json("json/spotify-2000-clean.json", orient="records")
 
-raise SystemExit # we will use weka or do training testing as needed
+raise SystemExit  # we will use weka or do training testing as needed
 # create training and testing data sets
 training_test_list = train_test_split(
     spotify_data, shuffle=True, train_size=0.70, test_size=0.30)
